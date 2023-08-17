e027a19b518977a61accd600bfcfa3654f518186
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-08-17 09:32:18 +0600

    text alignment added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// text style
 document.getElementById('bold-btn').addEventListener('click',function(){
     const textArea = document.getElementById('text-area')
     if(textArea.style.fontWeight ==='bold'){
@@ -10,7 +12,7 @@ document.getElementById('bold-btn').addEventListener('click',function(){
 document.getElementById('italic-btn').addEventListener('click',function(){
     const textArea = document.getElementById('text-area')
     if(textArea.style.fontStyle ==='italic'){
-        textArea.style.fontStyle =''
+        textArea.style.fontStyle ='normal'
     }
     else{
         textArea.style.fontStyle='italic'
@@ -24,4 +26,21 @@ document.getElementById('underline-btn').addEventListener('click',function(){
     else{
         textArea.style.textDecoration='underline'
     }
+})
+// text align
+document.getElementById('left-btn').addEventListener('click',function(){
+    const textArea =document.getElementById('text-area')
+    textArea.style.textAlign ="left"
+})
+document.getElementById('center-btn').addEventListener('click',function(){
+    const textArea =document.getElementById('text-area')
+    textArea.style.textAlign ="center"
+})
+document.getElementById('right-btn').addEventListener('click',function(){
+    const textArea =document.getElementById('text-area')
+   textArea.style.textAlign ="right"
+})
+document.getElementById('justify-btn').addEventListener('click',function(){
+    const textArea =document.getElementById('text-area')
+   textArea.style.textAlign ="justify"
 })
